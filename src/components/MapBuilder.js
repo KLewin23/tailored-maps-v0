7111fb84f094cfb7e6930f5b6a393db83d190b45
Kieran Lewin <kieran.lewin-cic-uk@ibm.com>
2020-02-24 14:52:44 +0000

    push

--- src/components/MapBuilder.js
+++ src/components/MapBuilder.js
@@ -1,10 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { GLView } from 'expo-gl'
 import ExpoTHREE, { THREE } from 'expo-three'
 import TouchControls from './TouchControls'
+import { Text } from '@ui-kitten/components'
+import { Animated } from 'react-native'
 
 export default function MapBuilder() {
-	const onContextCreate = async gl => {
+	const [speed, setSpeed] = useState(100)
+	const [startPosition, setStartPos] = useState([0, 0])
+
+	// const onContextCreate = async gl => {
+	// 	// const raycaster = new THREE.Raycaster()
+	// 	const scene = new THREE.Scene()
+	// 	const camera = new THREE.PerspectiveCamera(
+	// 		75,
+	// 		gl.drawingBufferWidth / gl.drawingBufferHeight,
+	// 		0.1,
+	// 		1000
+	// 	)
+	// 	const renderer = new ExpoTHREE.Renderer({ gl })
+	// 	renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight)
+	//
+	// 	// const geometry = new THREE.BoxGeometry(1, 1, 1)
+	// 	// const material = new THREE.MeshNormalMaterial({ wireframe: true })
+	// 	// const cube = new THREE.Mesh(geometry, material)
+	//
+	// 	// raycaster.setFromCamera(
+	// 	// 	{ x: startPosition[0], y: startPosition[1] },
+	// 	// 	camera
+	// 	// )
+	// 	//
+	// 	// const dist = cube.position
+	// 	// 	.clone()
+	// 	// 	.sub(camera.position)
+	// 	// 	.length()
+	//
+	// 	// raycaster.ray.at(dist, cube.position)
+	//
+	// 	scene.add(cube)
+	//
+	// 	const cube = new THREE.Mesh(
+	// 		new THREE.BoxBufferGeometry(),
+	// 		new THREE.MeshBasicMaterial({
+	// 			color: 'red',
+	// 			wireframe: true
+	// 		})
+	// 	)
+	//
+	// 	cube.rotation.x += speed
+	// 	cube.rotation.y += speed
+	//
+	// 	camera.position.set(0, 0, 10)
+	//
+	// 	console.log(startPosition)
+	//
+	// 	const animate = () => {
+	// 		console.log(speed)
+	// 		window.requestAnimationFrame(animate)
+	// 		renderer.render(scene, camera)
+	// 		gl.endFrameEXP()
+	// 	}
+	// 	animate()
+	// }
+
+	const _onGLContextCreate = async gl => {
 		const scene = new THREE.Scene()
 		const camera = new THREE.PerspectiveCamera(
 			75,
@@ -12,36 +71,39 @@ export default function MapBuilder() {
 			0.1,
 			1000
 		)
-		const renderer = new ExpoTHREE.Renderer({ gl })
+		const renderer = ExpoTHREE.createRenderer({ gl })
 		renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight)
 
-		const geometry = new THREE.BoxGeometry(1, 1, 1)
-		const material = new THREE.MeshNormalMaterial({ wireframe: true })
-		const cube = new THREE.Mesh(geometry, material)
-		scene.add(cube)
-
-		camera.position.y = 0
-		camera.position.x = 0
-		camera.position.z = 5
-
-		const animate = () => {
-			window.requestAnimationFrame(animate)
-			cube.rotation.x += 0.02
-			cube.rotation.y += 0.02
+		const geometry = new THREE.SphereBufferGeometry(1, 36, 36)
+		const material = new THREE.MeshBasicMaterial({
+			color: 'red',
+			wireframe: true
+		})
+		const sphere = new THREE.Mesh(geometry, material)
+		scene.add(sphere)
+		camera.position.set(0, 0, 10)
+		const render = () => {
+			window.requestAnimationFrame(render)
+			sphere.rotation.x += 0.01
+			sphere.rotation.y += 0.01
 			renderer.render(scene, camera)
 			gl.endFrameEXP()
 		}
-		animate()
+		render()
 	}
 
 	return (
-		<TouchControls
-			handleVelocityChange={console.log}
-		>
-			<GLView
-				style={{ flex: 1, backgroundColor: 'black' }}
-				onContextCreate={onContextCreate}
-			/>
-		</TouchControls>
+		<GLView
+			style={{ flex: 1, backgroundColor: 'black' }}
+			onContextCreate={_onGLContextCreate}
+		/>
 	)
 }
+
+//<Animated.View style={{ flex: 1 }}>
+
+// <TouchControls speedChange={setSpeed} startPos={setStartPos}>
+//{/*	<Text style={{ color: 'pink', position: 'absolute' }}>*/}
+// 		{/*		{startPosition}*/}
+// 		{/*	</Text>*/}
+// 		{/*</TouchControls>*/}
--- src/components/TouchControls.js
+++ src/components/TouchControls.js
@@ -1,18 +1,66 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { useState } from 'react'
+import { View, Alert } from 'react-native'
 import PropTypes from 'prop-types'
 
 function TouchControls(props) {
+	const [pressed, setPressed] = useState(false)
+	const [touchStart, setTouchStart] = useState({})
+	const [speed, setSpeed] = useState(0.0)
+
+	function onMove({ nativeEvent }) {
+		if (!pressed) {
+			setPressed(true)
+			// console.log([nativeEvent.locationX, nativeEvent.locationY])
+			// console.log('pressed')
+			setTouchStart({
+				posX: nativeEvent.locationX,
+				posY: nativeEvent.locationY,
+				timestamp: nativeEvent.timestamp
+			})
+			props.startPos([nativeEvent.locationX, nativeEvent.locationY])
+			console.log([nativeEvent.locationX, nativeEvent.locationY])
+		}
+	}
+
+	function onRelease({ nativeEvent }) {
+		if (pressed) {
+			setPressed(false)
+		}
+		// velocity calc
+		// get width of invisible rectangle
+		const width =
+			(nativeEvent.locationX > touchStart.posX
+				? nativeEvent.locationX
+				: touchStart.posX) -
+			(nativeEvent.locationX < touchStart.posX
+				? nativeEvent.locationX
+				: touchStart.posX)
+		// get length of invisible rectangle
+		const length =
+			(nativeEvent.locationY > touchStart.posY
+				? nativeEvent.locationY
+				: touchStart.posY) -
+			(nativeEvent.locationY < touchStart.posY
+				? nativeEvent.locationY
+				: touchStart.posY)
+		// get diagonal length of the invisible rectangle
+		const lineLength = Math.sqrt(width * width + (length + length))
+		const timeTaken = (nativeEvent.timestamp - touchStart.timestamp) / 1000
+		const initialSpeed = lineLength / timeTaken
+		setSpeed(initialSpeed)
+		// console.log([nativeEvent.locationX, nativeEvent.locationY])
+		// console.log('release')
+		props.speedChange(initialSpeed)
+	}
+
 	return (
 		<View
-			// onClick={() => {
-			// 	// some stuff
-			// 	const velocity = 1;
-			// 	return props.handleVelocityChange(velocity);
-			// }}
+			style={{ flex: 1 }}
 			onStartShouldSetResponder={() => {
-				console.log('xx')
+				return true
 			}}
+			onResponderMove={e => onMove(e)}
+			onResponderRelease={e => onRelease(e)}
 		>
 			{props.children}
 		</View>
@@ -20,7 +68,7 @@ function TouchControls(props) {
 }
 
 TouchControls.propTypes = {
-	children: PropTypes.arrayOf(PropTypes.object)
+	children: PropTypes.array
 }
 
 export default TouchControls
